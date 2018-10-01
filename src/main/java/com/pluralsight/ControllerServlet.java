a7ddd62d39014131e58926ad8fe180bd777fba61
Barbara <barbarabakker@hotmail.nl>
2018-10-01 15:46:45 +0200

    aef

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -122,14 +122,14 @@ public class ControllerServlet extends HttpServlet {
 
 	}
 
-	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+	/*private void showEditForm(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
 		Book existingBook = bookDAO.getBook(id);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm,jsp");
 		request.setAttribute("book", existingBook);
 		dispatcher.forward(request, response);
-	}
+	}*/
 
 
 
