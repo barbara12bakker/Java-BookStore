63fec912c64ac6b23aaaf0a731c47851e499a543
Barbara <barbarabakker@hotmail.nl>
2018-10-03 11:47:48 +0200

    andere code proberen, werkt goed

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -109,6 +109,13 @@ public class BookDAO {
                        "WHERE id = ?";
         try {
             PreparedStatement statement = jdbcConnection.prepareStatement(SQL);
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getAuthor());
+            statement.setString(3, book.getPrice());
+            statement.setString(4, book.getId());
+
+            statement.executeUpdate();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
